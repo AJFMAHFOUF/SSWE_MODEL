84ed1012055e069c89ba8990ae5117b5ca3e0023
AJFMAHFOUF <ajfmahfouf.meteo@gmail.com>
2023-10-08 16:54:09 +0200

    Modification to plotting script

--- tools/plot_swe_output3.py
+++ tools/plot_swe_output3.py
@@ -2,15 +2,14 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt 
 import numpy as np
 nlats=48 ; nlons=97
-nstep='0120'
+nstep='0000'
 trunc='031'
 expid='002'
-var='qv'
-var='uv'
+var='vor'
 plt.figure(figsize=(12,6))
 map = Basemap(projection='cyl',llcrnrlat=-90,urcrnrlat=90,\
             llcrnrlon=0,urcrnrlon=360,resolution='c')
-#map = Basemap(projection='ortho',lat_0=40,lon_0=-90,resolution='l')  
+map = Basemap(projection='ortho',lat_0=40,lon_0=-90,resolution='l')  
 #map = Basemap(projection='stere',width=18000000,height=18000000,lat_0=90.0,lon_0=-90.0,lat_ts=60.0,resolution='l')
 map.drawcoastlines(linewidth=0.25)
 map.drawcountries(linewidth=0.25)
@@ -39,10 +38,11 @@ if var == 'qv':
 lon2 = lon1.reshape((nlats,nlons))
 lat2 = lat1.reshape((nlats,nlons))
 x,y = map(lon2,lat2)
-if var == 'phi':
+if var == 'vor':
 	#cs = map.contourf(x,y,field/9.81,levels=np.arange(4600,6200,100),cmap='jet')	
-	cs = map.contour(x,y,field/98.1,levels=np.arange(460,590,10),colors='black')
-	cs = map.contourf(x,y,field/98.1,levels=np.arange(460,590,10),cmap='jet')
+	#cs = map.contour(x,y,field/98.1,levels=np.arange(460,590,10),colors='black')
+	#cs = map.contour(x,y,field/9.81,np.arange(4700,6100,100),colors='black')
+	cs = map.contourf(x,y,field,15,cmap='seismic')
 	#cs = map.contour(x,y,(field)/9.81+4450,levels=np.arange(9150,10300,50),colors='black')
 	#cs = map.contourf(x,y,(field)/9.81+4450,levels=np.arange(9150,10300,50),cmap='jet')
 	#cs = map.contourf(x,y,field/9.81,15,cmap='jet')
@@ -50,7 +50,7 @@ else:
 	z = np.sqrt(field1*field1 + field2*field2)
 	#cs = map.contourf(x,y,field,10,cmap='jet')
 	map.quiver(x[::1,::1],y[::1,::1],field1[::1,::1],field2[::1,::1],z[::1,::1],width=0.002,headwidth=2,scale=2,scale_units='xy',cmap='jet')
-#plt.colorbar(shrink=0.5)
-#plt.title(var+' 500 hpa '+nstep+'h - T'+trunc+' expid='+expid)
-#plt.savefig('../plots/'+var+'_500_T'+trunc+'_step_'+nstep+'_expid_'+expid+'_NH.pdf')
+plt.colorbar(shrink=0.5)
+plt.title('SWE '+var+' 500 hpa '+nstep+'h - T'+trunc+' expid='+expid)
+plt.savefig('../plots/SWE_'+var+'_500_T'+trunc+'_step_'+nstep+'_expid_'+expid+'_NH.pdf')
 plt.show()
